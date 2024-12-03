fe6e3be79a38f13282bde9eb1fc199224c339227
Dipak DC <141993316+dpac14@users.noreply.github.com>
2024-12-03 14:03:49 +0530

    Add files via upload

---
+++ Javascript_Basics/02_data.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all JS code as newer version
+// alert ( 3 + 3) we are using nodejs , not browser 
+
+let name ="dpak"
+let age ="19"
+let isLoggeIn = false
+// number=> 2 to power
+// bigint
+// string=>""
+//boolean=> true/fales
+//null=>standalone value
+//undefined=>
