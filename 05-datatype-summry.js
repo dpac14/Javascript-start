ff386fe4987aa094be918de4ddac6ea6583037e6
dpac14 <dipakchute11@gmail.com>
2024-12-06 13:23:43 +0530

    1st commit

---
+++ 05-datatype-summry.js
@@ -0,0 +1,26 @@
+// Primitive 
+// type : String , number, boolearn,null, undefined, symbol,bigInt
+  const score = 100
+  const scoreValue =100.3
+   const isLoggedIn =false
+   const outsideTemp =null 
+   let userEmail;
+
+   const id =Symbol ("123")
+   const anotherId = Symbol("123")
+    // console.log(id === anotherId);
+
+
+//Reference (no Primitive)
+// Array, Objects, Functions
+
+const heros =["shatiman" , "naagraj", "doga"];
+let myOBJ ={
+    name: "dipak",
+    age:21,
+}
+
+const myFunction =function(){
+    console.log("hellow world");
+    
+}
