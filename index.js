34a7a54362bbe1c77af6ebc9b656e4c739e2714f
dpac14 <dipakchute11@gmail.com>
2024-12-10 12:30:21 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// const arr =[2, 3, 4, 5]
+// // console.log(arr);
+// // console.log(arr.slice(0,1));
+
+// arr.splice(0,1)
+// arr.unshift(8)
+// console.log(arr);
+
+
+// const obj={
+//     a : 1,
+//     b : 2,
+//     c : 3,
+//     "a" : 6,
+//     b : 7,
+// }
+// console.log(obj);
+
+
+console.log(!"hellow");
+console.log(+false);
+
