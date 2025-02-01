66b5fb7b12e9effc8ca3c279452f197d633656c2
Dipak DC <141993316+dpac14@users.noreply.github.com>
2025-02-01 13:22:38 +0000

    New Changes

--- 01Js_basics/datatype_summry.js
+++ 01Js_basics/datatype_summry.js
@@ -1,26 +1,55 @@
 // Primitive 
-// type : String , number, boolearn,null, undefined, symbol,bigInt
-  const score = 100
-  const scoreValue =100.3
-   const isLoggedIn =false
-   const outsideTemp =null 
-   let userEmail;
+//  Datatype : String, Symbol, Number, Null, Boolearn, BigInt, Undefined, 
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
 
-   const id =Symbol ("123")
-   const anotherId = Symbol("123")
-    console.log(id === anotherId);
+const id = Symbol("123")
+const anotherId = Symbol("123")
+// console.log(id === anotherId);
 
+ const bigNumber = 99990726458930n
 
-//Reference (no Primitive)
+//Reference (Non Primitive)
 // Array, Objects, Functions
 
-const heros =["shatiman" , "naagraj", "doga"];
-let myOBJ ={
+const heros = ["shatiman", "naagraj", "doga"];
+let myObj = {
     name: "dipak",
-    age:21,
+    age: 21,
 }
 
-const myFunction =function(){
+const myFunction = function () {
     console.log("hellow world");
-    
+
 }
+console.log( typeof bigNumber);
+
+
+
+//**********************************************************//
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "dpakkk"
+
+let anothername = myYoutubename
+
+anothername = "chai or code"
+// console.log(myYoutubename);
+// console.log(anothername);
+
+
+
+let userOne = {
+    email: "user@gcom",
+    upi: "user@ypl",
+}
+
+let userTow = userOne
+
+userTow.email = "diapk@gcom"
+// console.log(userOne.email);
+// console.log(userTow.email);
